c9d03a065fff631afcfe40e31daa217996bd5535
Suheer Zahid <suheer.work18@gmail.com>
2024-04-25 02:54:54 +0500

    ...

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useRef } from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import TawkMessengerReact from '@tawk.to/tawk-messenger-react';
-import HomePage from './pages/home/HomePage'
-import Footer from './components/Footer'
+import TawkMessengerReact from "@tawk.to/tawk-messenger-react";
+import { FaWhatsapp } from "react-icons/fa";
+import HomePage from "./pages/home/HomePage";
+import Footer from "./components/Footer";
 import Contact from "./pages/contact/Contact";
 import About from "./pages/about/About";
 import Samples from "./pages/samples/Samples";
@@ -11,7 +12,7 @@ import Assignments from "./pages/services/Assignments";
 import OnlineClasses from "./pages/services/OnlineClasses";
 import NotFound from "./components/NotFound";
 import Header from "./components/Header";
-import './App.css'
+import "./App.css";
 import PrivacyPolicy from "./pages/privacyPolicy/PrivacyPolicy";
 import Dummy from "./components/Dummy";
 
@@ -26,6 +27,9 @@ function App() {
     // console.log('onLoad works!');
   };
 
+  const openWhatsApp = () => {
+    window.open("https://wa.me/yourPhoneNumberHere", "_blank"); // Replace "yourPhoneNumberHere" with your WhatsApp number
+  };
 
   return (
     <>
@@ -34,7 +38,8 @@ function App() {
           propertyId="646da861ad80445890eec388"
           widgetId="1h165jg27"
           ref={tawkMessengerRef}
-          onLoad={onLoad} />
+          onLoad={onLoad}
+        />
 
         <Header />
         <Routes>
@@ -51,11 +56,21 @@ function App() {
           <Route path="/online-classes" element={<OnlineClasses />} />
         </Routes>
 
+        {/* WhatsApp button */}
+        <a
+          href="https://api.whatsapp.com/send?phone=17163749747"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="whatsapp-btn"
+        >
+          <FaWhatsapp />
+        </a>
+
         {/* <Dummy /> */}
         <Footer />
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
